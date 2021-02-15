6873500fe6d5533e0f2425b15ce54537264fedf5
jlee0425 <jake.jklee425@gmail.com>
2021-02-15 19:00:59 +0900

    nextjs

---
+++ pages/cars/[id].js
@@ -0,0 +1,61 @@
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+
+const Car = ({ car }) => {
+	const router = useRouter();
+	const { id } = router.query;
+
+	return (
+		<>
+			<Head>
+				<title>
+					{car.color} {car.id}
+				</title>
+			</Head>
+			<main>
+				<h1>Hello {id}</h1>
+				<img src={car.image} width='300px' />
+			</main>
+		</>
+	);
+};
+
+// SSR
+export const getServerSideProps = async ({ params }) => {
+	const req = await fetch(`http://localhost:3000/${params.id}.json`);
+	const data = await req.json();
+
+	return {
+		props: { car: data },
+	};
+};
+
+// SSG
+// export const getStaticProps = async ({ params }) => {
+// 	const req = await fetch(`http://localhost:3000/${params.id}.json`);
+// 	const data = await req.json();
+
+// 	return {
+// 		props: { car: data },
+// 	};
+// };
+
+// export const getStaticPaths = async () => {
+// 	const req = await fetch('http://localhost:3000/cars.json');
+// 	const data = await req.json();
+
+// 	const paths = data.map((car) => {
+// 		return {
+// 			params: {
+// 				id: car,
+// 			},
+// 		};
+// 	});
+
+// 	return {
+// 		paths,
+// 		fallback: false,
+// 	};
+// };
+
+export default Car;
